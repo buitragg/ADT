dec22a536938382b217823a8365e68ce16103a46
buitragg <gmbuitrago10@gmail.com>
2025-05-02 15:17:40 -0700

    added remove all capabilities for LinkedList recursively

--- LinkedList_recursive.py
+++ LinkedList_recursive.py
@@ -32,34 +32,45 @@ class LinkedList:
         :param value: value to be added
         :return: None
         """
+        # if the list is empty add the value as the head
         if self._head is None:
             self._head = Node(value)
             return
         self.helper_add(self._head, value)
 
-
-    def helper_remove(self, a_node, value):
+    def helper_remove(self, a_node, prev, value):
         """
-        removes one instance of the value provided
-        :param a_node:
-        :param value:
-        :return:
+        removes all instances of a value provided
+        :param a_node: keeps track of current node
+        :param value: value to be removed
+        :param prev: keeps track of previous node
+        :return: None
         """
+        # if we reached the end of the list
         if a_node is None:
             return
-        elif a_node.next.data == value:
-            a_node.next = a_node.next.next
+        if a_node.next is None and a_node.data != value:
+            return
         else:
-            self.helper_remove(a_node.next, value)
+            # if we are in the last node, and we have the value
+            if a_node.data == value and a_node.next is None:
+                prev.next = a_node.next
+                return
+            # any other node with the value
+            else:
+                prev.next = a_node.next
+                self.helper_remove(a_node.next, prev, value)
 
-
+        self.helper_remove(a_node.next, a_node, value)
 
     def remove(self, value):
+        if self._head is None:
+            return
+        # if it's a list of all same value to be removed
         if self._head.data == value:
             self._head = self._head.next
-            return
-        self.helper_remove(self._head, value)
-
+            self.remove(value)
+        self.helper_remove(self._head, None, value)
 
     def helper_display(self, a_node):
         """recursive display method"""
@@ -70,15 +81,14 @@ class LinkedList:
 
     def display(self):
         self.helper_display(self._head)
-        
+
 
 l = LinkedList()
-l.add(1)
+l.add(2)
+l.add(2)
+l.add(2)
 l.add(2)
 l.add(2)
 l.display()
 l.remove(2)
 l.display()
-l.add(4)
-l.display()
-
